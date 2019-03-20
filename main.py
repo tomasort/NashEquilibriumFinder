f48565a1abe423e6a48fecf63c928581aba6b195
Tomas Ortega <torte007@fiu.edu>
2019-03-20 13:13:35 -0400

    added header

---
+++ main.py
@@ -0,0 +1,19 @@
+#  =============================================================================
+#  Group	Number: This	number	will	be	assign	to	you	in	canvas	in	the	grades	section
+# PROGRAMMER1: Tomas Ortega
+#  PANTHER	ID1: 5677483
+ 
+# PROGRAMMER2: Pablo Mueller
+#  	PANTHER	ID2: Your	panther	ID
+ 
+#  	CLASS: CAP4506	
+#  	SECTION: U01
+#  	SEMESTER: Spring 2019
+#  	CLASSTIME: T/TH	6:25-7:45 PM
+ 
+#  	Project: This program will alow the user to find nash equilibriums and calculate expected payoffs for each player. 
+#  	DUE: Sunday, March	31,	2019 at midnight.							
+ 
+#  	CERTIFICATION:	 I	certify	that	this	work	is	my	own	and	that
+#  																															 none	of	it	is	the	work	of	any	other	person.
+#  =============================================================================
